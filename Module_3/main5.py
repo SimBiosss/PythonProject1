429b76c06e27ee4743cfac6fa63e0f5b6585ff99
SimBiosss <evdockimoffd@yandex.ru>
2024-12-22 14:57:40 +0300

    22.12.2024

--- Module_3/main5.py
+++ Module_3/main5.py
@@ -2,6 +2,8 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
+        if int(str_number[1:]) == 0:
+            return first
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
