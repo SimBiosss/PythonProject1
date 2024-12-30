2be149bb1bae9625491e8845c871c5e7741fd102
SimBiosss <evdockimoffd@yandex.ru>
2024-12-30 22:53:47 +0300

    30.12.2024

---
+++ Module_4/fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    if second != 0:
+        result = first / second
+        return result
+    else:
+        result = "Ошибка"
+    return result
+
+# first_number = int(input("Первое число: "))
+# second_number = int(input("Второе число: "))
+# divide(first_number, second_number)
---
+++ Module_4/true_math.py
@@ -0,0 +1,12 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        result = first / second
+        return result
+    else:
+        return inf
+
+# first_number = int(input("Первое число: "))
+# second_number = int(input("Второе число: "))
+# divide(first_number, second_number)
